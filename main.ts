47e083f20194bc999dc5b10d877e18fd5f7d32c8
thebeast102 <77120888+thebeast102@users.noreply.github.com>
2021-01-26 17:18:27 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("loading...")
     basic.showNumber(3)
-    basic.pause(1000)
+    basic.pause(5000)
     basic.showNumber(2)
-    basic.pause(1000)
+    basic.pause(5000)
     basic.showNumber(1)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    servos.P1.setAngle(180)
+    basic.pause(300)
+    servos.P1.setAngle(90)
     basic.pause(1000)
+    servos.P2.setAngle(0)
+    basic.pause(1000)
+    servos.P2.setAngle(90)
+    basic.pause(1000)
+    servos.P1.setAngle(0)
+    basic.pause(300)
+    servos.P1.setAngle(90)
+    basic.pause(500)
+    servos.P2.setAngle(180)
+    basic.pause(1000)
+    servos.P2.setAngle(90)
+    basic.pause(1600)
+})
+input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         # # . # #
         # # . # #
